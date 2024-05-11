105b82fc3f30fd48dee254f7201154a1e740d037
Enes Civelek <enescvlk11@hotmail.com>
2024-05-11 20:09:58 +0300

    Part 1 tamamlandı

---
+++ UdemyMyPortfolio/DAL/Context/MyPortfolioContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+using UdemyMyPortfolio.DAL.Entites;
+
+namespace UdemyMyPortfolio.DAL.Context
+{
+	public class MyPortfolioContext:DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("Server=ENES;initial Catalog=UdemyMyPortfolio;" +
+				"integrated Security=true;");
+		}
+		public DbSet<About> Abouts { get; set; }
+		public DbSet<Contact> Contacts { get; set; }
+		public DbSet<Experience> Experiences { get; set; }
+		public DbSet<Feature> Features { get; set; }
+		public DbSet<Message> Messages { get; set; }
+		public DbSet<Portfolio> Portfolios { get; set; }
+		public DbSet<Skill> Skills { get; set; }
+		public DbSet<SocialMedia> SocialMedias { get; set; }
+		public DbSet<Testimonial> Testimonials { get; set; }
+	}
+}
---
+++ UdemyMyPortfolio/ViewComponents/_FeatureComponentPartial.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UdemyMyPortfolio.DAL.Context;
+
+namespace MyPortolioUdemy.ViewComponents
+{
+    public class _FeatureComponentPartial:ViewComponent
+    {
+        MyPortfolioContext context = new MyPortfolioContext();
+        public IViewComponentResult Invoke()
+        {
+            var values = context.Features.ToList();
+            return View(values);
+        }
+    }
+}
